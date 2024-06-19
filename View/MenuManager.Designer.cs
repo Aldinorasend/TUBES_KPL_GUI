5327a7228361a2199c18700570e22b9cbe27035d
Aldinorasend <aldinorasend@gmail.com>
2024-06-19 23:46:21 +0700

    MenuManager by Aldino

---
+++ View/MenuManager.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace View
+{
+    partial class MenuManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.IdMenu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nama = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Harga = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtHarga = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdMenu,
+            this.Nama,
+            this.Harga});
+            this.dataGridView1.Location = new System.Drawing.Point(125, 19);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(591, 231);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // IdMenu
+            // 
+            this.IdMenu.HeaderText = "Id Menu";
+            this.IdMenu.MinimumWidth = 8;
+            this.IdMenu.Name = "IdMenu";
+            // 
+            // Nama
+            // 
+            this.Nama.HeaderText = "Nama";
+            this.Nama.MinimumWidth = 8;
+            this.Nama.Name = "Nama";
+            // 
+            // Harga
+            // 
+            this.Harga.HeaderText = "Harga";
+            this.Harga.MinimumWidth = 8;
+            this.Harga.Name = "Harga";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(284, 277);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 35);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(109, 277);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(112, 35);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Insert";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtHarga
+            // 
+            this.txtHarga.Location = new System.Drawing.Point(216, 428);
+            this.txtHarga.Name = "txtHarga";
+            this.txtHarga.Size = new System.Drawing.Size(284, 26);
+            this.txtHarga.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 428);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Harga";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(216, 382);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(284, 26);
+            this.txtNama.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 382);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Nama";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(216, 333);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(284, 26);
+            this.txtId.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 333);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Id Menu";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(448, 277);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(112, 35);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(604, 277);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(112, 35);
+            this.btnClear.TabIndex = 20;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // MenuManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 488);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtHarga);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdate);
+            this.Name = "MenuManager";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdMenu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nama;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Harga;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtHarga;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ View/MenuManager.cs
@@ -0,0 +1,246 @@
+﻿using Main.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Menu = Main.Model.Menu;
+
+namespace View
+{
+    public partial class MenuManager : Form
+    {
+        public string fileDataPathMenu = "D:\\C# Project\\TUBES_GUI_RESTO_KPL_NEWEST\\Main\\Data\\dataMenu.json";
+
+        public MenuManager()
+        {
+            InitializeComponent();
+            PopulateDataGridView();
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                // Mendapatkan nilai dari sel dalam baris yang diklik
+                string IdMenu = row.Cells[0].Value?.ToString() ?? string.Empty;
+                string namaMenu = row.Cells[1].Value?.ToString() ?? string.Empty;
+
+                // Penanganan konversi Harga
+                int Harga = 0;
+                if (row.Cells[2].Value != null && int.TryParse(row.Cells[2].Value.ToString(), out int parsedHarga))
+                {
+                    Harga = parsedHarga;
+                }
+
+                // Menetapkan nilai ke TextBox
+                txtId.Text = IdMenu;
+                txtNama.Text = namaMenu;
+                txtHarga.Text = Harga.ToString();
+
+               
+            }
+        }
+
+        private void PopulateDataGridView()
+        {
+
+            dataGridView1.Rows.Clear();
+            List<Menu> daftarMenu = ReadJsonFile(fileDataPathMenu);
+
+            foreach (var menu in daftarMenu)
+            {
+                dataGridView1.Rows.Add(menu.IdMenu, menu.Nama, menu.Harga);
+            }
+        }
+
+        private List<Menu> ReadJsonFile(string fileDataPathMenu)
+        {
+            List<Menu> daftarMenu = new List<Menu>();
+
+            try
+            {
+                if (string.IsNullOrEmpty(fileDataPathMenu))
+                {
+                    throw new ArgumentException("File path cannot be null or empty.", nameof(fileDataPathMenu));
+                }
+
+                string json;
+                using (StreamReader reader = new StreamReader(fileDataPathMenu))
+                {
+                    json = reader.ReadToEnd();
+                }
+
+                daftarMenu = JsonSerializer.Deserialize<List<Menu>>(json);
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"File not found: {ex.Message}");
+            }
+            catch (System.Text.Json.JsonException ex)
+            {
+                MessageBox.Show($"Error parsing JSON file: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error reading JSON file: {ex.Message}");
+            }
+
+            return daftarMenu;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearText();
+        }
+
+        private void ClearText()
+        {
+            txtHarga.Clear();
+            txtId.Clear();
+            txtNama.Clear();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            String idMenu = txtId.Text;
+            String nama = txtNama.Text;
+            int harga = int.Parse(txtHarga.Text);
+            List<Menu> dtmenu = ReadJSON();
+            bool statusMenu = Validate(idMenu);
+
+            if (statusMenu)
+            {
+                MessageBox.Show("Id Menu telah digunakan");
+            }
+            else
+            {
+                Menu dtMn = new Menu(idMenu, nama, harga);
+                dtmenu.Add(dtMn);
+                WriteJSON(dtmenu);
+                MessageBox.Show("Menu berhasil ditambahkan");
+
+                // Refresh DataGridView
+                PopulateDataGridView();
+            }
+        }
+
+        private bool Validate(string idMenu)
+        {
+            List<Menu> dtMenu = ReadJSON();
+            for (int i = 0; i < dtMenu.Count; i++)
+            {
+                if (dtMenu[i].IdMenu.Equals(idMenu))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void WriteJSON(List<Menu> dtmenu)
+        {
+
+         string fileDataPathMenu = "D:\\C# Project\\TUBES_GUI_RESTO_KPL_NEWEST\\Main\\Data\\dataMenu.json";
+        JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            string jsonString = JsonSerializer.Serialize(dtmenu, options);
+            File.WriteAllText(fileDataPathMenu, jsonString);
+        }
+
+        private List<Menu> ReadJSON()
+        {
+
+            string fileDataPathMenu = "D:\\C# Project\\TUBES_GUI_RESTO_KPL_NEWEST\\Main\\Data\\dataMenu.json";
+            List<Menu> Data = new List<Menu>();
+            try
+            {
+                string configjsonData = File.ReadAllText(fileDataPathMenu);
+                Data = JsonSerializer.Deserialize<List<Menu>>(configjsonData);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error " + ex.Message);
+
+
+            }
+            return Data;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            List<Menu> list = ReadJSON();
+            string idMenu = txtId.Text;
+            if (string.IsNullOrEmpty(idMenu))
+            {
+                MessageBox.Show("Masukkan Nama Barang");
+            }
+            else
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (idMenu.Equals(list[i].IdMenu))
+                    {
+                        list.Remove(list[i]);
+                        WriteJSON(list);
+                        MessageBox.Show("Menu terhapus!");
+                        ClearText();
+                        PopulateDataGridView();
+                    }
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            List<Menu> list = ReadJSON();
+            string idMenu = txtId.Text;
+            string nama = txtNama.Text;
+            int harga = int.Parse(txtHarga.Text);
+
+            if (string.IsNullOrEmpty(idMenu))
+            {
+                MessageBox.Show("Masukkan Id Menu");
+            }
+            else
+            {
+                bool found = false;
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (idMenu.Equals(list[i].IdMenu))
+                    {
+                        list[i].Nama = nama;
+                        list[i].Harga = harga;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    WriteJSON(list);
+                    MessageBox.Show("Menu berhasil diupdate!");
+                    ClearText();
+                    PopulateDataGridView();
+                }
+                else
+                {
+                    MessageBox.Show("Id Menu tidak ditemukan!");
+                }
+            }
+        }
+    }
+}
