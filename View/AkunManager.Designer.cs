451d52b231060a336db8647fb558769b3f55991d
xaveriusabel <xaveriusabel@gmail.com>
2024-06-24 02:29:23 +0700

    DataAkun Dengan implementasi Secure Code dan Clean Code

---
+++ View/AkunManager.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace View
+{
+    partial class AkunManager
+    {
+        // Required designer variable
+        private System.ComponentModel.IContainer components = null;
+
+        // Clean up any resources being used
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        // Required method for Designer support - do not modify
+        // the contents of this method with the code editor
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUname = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.Username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Password = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nama = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Username,
+            this.Password,
+            this.Nama});
+            this.dataGridView1.Location = new System.Drawing.Point(43, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(698, 189);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(345, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data Akun";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Username ";
+            // 
+            // txtUname
+            // 
+            this.txtUname.Location = new System.Drawing.Point(140, 280);
+            this.txtUname.Name = "txtUname";
+            this.txtUname.Size = new System.Drawing.Size(223, 26);
+            this.txtUname.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(140, 318);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(223, 26);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password ";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(140, 361);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(223, 26);
+            this.txtName.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 361);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Name ";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(412, 273);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(89, 40);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(522, 273);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(89, 40);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(631, 273);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(89, 40);
+            this.btnBack.TabIndex = 10;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // Username
+            // 
+            this.Username.HeaderText = "Username";
+            this.Username.MinimumWidth = 8;
+            this.Username.Name = "Username";
+            // 
+            // Password
+            // 
+            this.Password.HeaderText = "Password";
+            this.Password.MinimumWidth = 8;
+            this.Password.Name = "Password";
+            // 
+            // Nama
+            // 
+            this.Nama.HeaderText = "Name";
+            this.Nama.MinimumWidth = 8;
+            this.Nama.Name = "Nama";
+            // 
+            // AkunManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "AkunManager";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUname;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Username;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Password;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nama;
+    }
+}
---
+++ View/AkunManager.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.IO;
+
+using System.Text.Json;
+
+using System.Windows.Forms;
+using Main.Model;
+
+namespace View
+{
+    public partial class AkunManager : Form
+    {
+        public String fileDataPathAkun = Path.Combine(System.Windows.Forms.Application.StartupPath, "Data", "dataAkun.json");
+        public AkunManager()
+        {
+            InitializeComponent();
+            PopulateDataGridView();
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                string Username = row.Cells[0].Value.ToString();
+                string Password = row.Cells[1].Value.ToString();
+                string Nama = row.Cells[2].Value.ToString();
+
+
+                Akun akn = new Akun(Username, Password, Nama);
+                txtUname.Text = akn.Username;
+                txtPassword.Text = akn.Password;
+                txtName.Text = akn.Nama;
+
+
+            }
+
+        }
+
+        private void PopulateDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+            List<Akun> daftarAkun = ReadJsonFile(fileDataPathAkun);
+
+            foreach (var Akun in daftarAkun)
+            {
+                dataGridView1.Rows.Add(Akun.Username, Akun.Password, Akun.Nama);
+            }
+        }
+        private List<Akun> ReadJsonFile(string filePath)
+        {
+            List<Akun> daftarAkun = new List<Akun>();
+
+            try
+            {
+                if (string.IsNullOrEmpty(filePath))
+                {
+                    throw new ArgumentException("File path cannot be null or empty.", nameof(filePath));
+                }
+
+                string json;
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    json = reader.ReadToEnd();
+                }
+
+                daftarAkun = JsonSerializer.Deserialize<List<Akun>>(json);
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"File not found: {ex.Message}");
+            }
+            catch (JsonException ex)
+            {
+                MessageBox.Show($"Error parsing JSON file: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error reading JSON file: {ex.Message}");
+            }
+
+            return daftarAkun;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtUname.Clear();
+            txtPassword.Clear();
+            txtName.Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                string usernameToDelete = selectedRow.Cells[0].Value.ToString();
+
+                // Read the current list of Akun from the JSON file
+                List<Akun> daftarAkun = ReadJsonFile(fileDataPathAkun);
+
+                // Remove the Akun with the selected username
+                daftarAkun.RemoveAll(a => a.Username == usernameToDelete);
+
+                // Serialize the updated list back to the JSON file
+                string updatedJson = JsonSerializer.Serialize(daftarAkun);
+                File.WriteAllText(fileDataPathAkun, updatedJson);
+
+                // Refresh the DataGridView
+                PopulateDataGridView();
+
+                // Clear the text boxes
+                btnClear_Click(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Please select a row to delete.");
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
